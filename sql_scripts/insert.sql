743a27b857eb08a787a0578b53990fb7173834e5
Anna Fadeeva <151925079+AnnaFad@users.noreply.github.com>
2025-01-11 15:00:46 +0300

    Update insert.sql

--- sql_scripts/insert.sql
+++ sql_scripts/insert.sql
@@ -2,7 +2,7 @@
 CREATE OR REPLACE FUNCTION AddChorister(p_first_name VARCHAR(200), p_last_name VARCHAR(200), p_is_conductor BOOLEAN, p_description TEXT, p_photo TEXT) 
     RETURNS VOID AS $$
 BEGIN
-    INSERT INTO choristers (first_name, last_name, is_conductor, description, photo, is_deleted, create_add, update_add) 
+    INSERT INTO choristers (first_name, last_name, is_conductor, description, photo, is_deleted, create_at, update_at) 
     VALUES (p_first_name, p_last_name, p_is_conductor, p_description, p_photo, False, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP) ;
 END;
 $$ LANGUAGE plpgsql;
@@ -11,7 +11,7 @@ $$ LANGUAGE plpgsql;
 CREATE OR REPLACE FUNCTION AddAdmin(p_email varchar(100), p_password_hash varchar(100)) 
     RETURNS VOID AS $$
 BEGIN
-    INSERT INTO users (email, password_hash, role_user, create_add, update_add) 
+    INSERT INTO users (email, password_hash, role_user, create_at, update_at) 
     VALUES (p_email, p_password_hash, 'Admin', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP) ;
 END;
 $$ LANGUAGE plpgsql;
@@ -20,7 +20,7 @@ $$ LANGUAGE plpgsql;
 CREATE OR REPLACE FUNCTION AddNews(p_title VARCHAR(400), p_text TEXT, p_photo TEXT) 
     RETURNS VOID AS $$
 BEGIN
-    INSERT INTO news(title, text_news, photo,  is_deleted, create_add, update_add) 
+    INSERT INTO news(title, text_news, photo,  is_deleted, create_at, update_at) 
     VALUES (p_title, p_text, p_photo, False, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP) ;
 END;
 $$ LANGUAGE plpgsql;
@@ -29,7 +29,7 @@ $$ LANGUAGE plpgsql;
 CREATE OR REPLACE FUNCTION AddEvent(p_name_event VARCHAR(500), p_description TEXT, p_event_time TIMESTAMP, p_photo TEXT, p_has_registration BOOLEAN, p_limit_people INT, p_date_time_open TIMESTAMP, p_hours_for_registration INT) 
     RETURNS VOID AS $$
 BEGIN
-    INSERT INTO events(name_event, description_, event_time, photo, has_registration, registration_is_open, limit_people, date_time_open, hours_for_registration, is_deleted, create_add, update_add) 
+    INSERT INTO events(name_event, description_, event_time, photo, has_registration, registration_is_open, limit_people, date_time_open, hours_for_registration, is_deleted, create_at, update_at) 
     VALUES (p_name_event, p_description, p_event_time, p_photo, p_has_registration, p_registration_is_open, p_limit_people, p_date_time_open, p_hours_for_registration, False, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
     
 END;
@@ -42,7 +42,7 @@ BEGIN
 	Update rehersals
     SET is_last = FALSE;
     	
-    INSERT INTO rehersals(title, date_start, days_for_registration, is_last, create_add, update_add) 
+    INSERT INTO rehersals(title, date_start, days_for_registration, is_last, create_at, update_at) 
     VALUES (p_title, p_date_start, p_days_for_registration, True, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP) ;
    	
 END;
@@ -55,7 +55,7 @@ $$ LANGUAGE plpgsql;
     BEGIN
     ASSERT (SELECT registration_is_open FROM events WHERE is_deleted = FALSE AND event.ID = p_event_ID) = True, 
     		'Невозможно добавить регистрацию на мероприятие id = %', p_event_ID;
-    INSERT INTO visitors(data_visitor, event_ID, is_deleted, create_add, update_add) 
+    INSERT INTO visitors(data_visitor, event_ID, is_deleted, create_at, update_at) 
     VALUES (p_data_visitor, p_event_ID,  False, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
     IF (SELECT COUNT(*) FROM visitors WHERE event_id = p_event_ID) >= (SELECT limit_people FROM events WHERE Id = p_event_ID)
     THEN UPDATE events
@@ -71,11 +71,7 @@ CREATE OR REPLACE FUNCTION AddApplicant(p_data_applicant jsonb)
     BEGIN
     ASSERT (SELECT date_start + INTERVAL'% days', days_for_registration FROM rehersals WHERE is_last = TRUE) < CURRENT_TIMESTAMP, 
     		'Невозможно добавить регистрацию на мероприятие id = %', p_event_ID;
-    INSERT INTO applicants(data_applicant, status, rehersal_id, create_add, update_add) 
+    INSERT INTO applicants(data_applicant, status, rehersal_id, create_at, update_at) 
     VALUES (p_data_applicant, 'New', (SELECT id From rehersals WHERE is_last = TRUE), CURRENT_TIMESTAMP, CURRENT_TIMESTAMP) ;
 	END;
 $$ LANGUAGE plpgsql;
-
-
-
-    
